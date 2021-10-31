9af4f90ccc2c2de47594583f5e5a4e0a77caa9a3
IJLAL AHMAD <ijlalahmad845@gmail.com>
2021-10-31 13:47:56 +0530

    added result cards data model class and optimised matrix cards to responsive on matrix size

---
+++ app/src/main/java/com/example/matscape/dataModels/ResultCards.java
@@ -0,0 +1,69 @@
+package com.example.matscape.dataModels;
+
+import java.util.List;
+
+public class ResultCards {
+
+    String expression;
+    String message;
+    List<List<String>> resultMatrix;
+    int matrixRows,matrixColumns;
+    double textSize;
+
+    public ResultCards(String expression, String message, List<List<String>> resultMatrix, int matrixRows, int matrixColumns, double textSize) {
+        this.expression = expression;
+        this.message = message;
+        this.resultMatrix = resultMatrix;
+        this.matrixRows = matrixRows;
+        this.matrixColumns = matrixColumns;
+        this.textSize = textSize;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<List<String>> getResultMatrix() {
+        return resultMatrix;
+    }
+
+    public void setResultMatrix(List<List<String>> resultMatrix) {
+        this.resultMatrix = resultMatrix;
+    }
+
+    public int getMatrixRows() {
+        return matrixRows;
+    }
+
+    public void setMatrixRows(int matrixRows) {
+        this.matrixRows = matrixRows;
+    }
+
+    public int getMatrixColumns() {
+        return matrixColumns;
+    }
+
+    public void setMatrixColumns(int matrixColumns) {
+        this.matrixColumns = matrixColumns;
+    }
+
+    public double getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(double textSize) {
+        this.textSize = textSize;
+    }
+}
