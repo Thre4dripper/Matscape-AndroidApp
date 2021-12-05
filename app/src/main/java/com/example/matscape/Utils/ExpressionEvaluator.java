4492a6500b067ea89c27273951c9179108bb9a88
IJLAL AHMAD <ijlalahmad845@gmail.com>
2021-12-05 21:48:39 +0530

    changed logic of getting String from Evaluation stack to getting 2d List

--- app/src/main/java/com/example/matscape/Utils/ExpressionEvaluator.java
+++ app/src/main/java/com/example/matscape/Utils/ExpressionEvaluator.java
@@ -4,63 +4,81 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.example.matscape.Controllers.MatrixCardsController;
+import com.example.matscape.Controllers.ResultCardsController;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class ExpressionEvaluator {
     private static final String TAG = "ExpressionEvaluator";
 
     public static void EvaluateExpression(@NonNull String postfixExpression, int selectedCard) {
-        Stack<String> stack = new Stack<>();
+        Stack<List<List<String>>> stack = new Stack<>();
         int flag = 0;
         char currentChar;
 
         for (int i = 0; i < postfixExpression.length(); i++) {
             currentChar = postfixExpression.charAt(i);
 
-            if ((Character.isDigit(currentChar) || currentChar=='.')) {
+            if (Character.isDigit(currentChar) || currentChar == '.') {
                 if (flag == 0) {
-                    stack.push(String.valueOf(currentChar));
+
+                    List<List<String>> element = new ArrayList<>();
+                    element.add(new ArrayList<>());
+                    element.get(0).add(String.valueOf(currentChar));
+
+                    stack.push(element);
                     flag = 1;
                 } else {
-                    String number = stack.pop();
-                    number += currentChar;
-                    stack.push(number);
+                    List<List<String>> element = stack.pop();
+                    element.get(0).set(0, element.get(0).get(0) + currentChar);
+                    stack.push(element);
                 }
             } else if (currentChar == ' ')
                 flag = 0;
 
             else if (Character.isUpperCase(currentChar))
-                stack.push(String.valueOf(currentChar));
+                stack.push(getCurrentMatrix(currentChar));
 
             else if (currentChar == '+' || currentChar == '-' || currentChar == '•' || currentChar == '/' || currentChar == '^') {
-                String num1 = stack.pop();
-                String num2 = stack.pop();
-                Double result;
+                List<List<String>> num1 = stack.pop();
+                List<List<String>> num2 = stack.pop();
+                List<List<String>> result = new ArrayList<>();
+                result.add(new ArrayList<>());
                 switch (currentChar) {
                     case '+':
-                        result = Double.parseDouble(num2) + Double.parseDouble(num1);
-                        stack.push(String.valueOf(result));
+                        result.get(0).add(String.valueOf(Double.parseDouble(num2.get(0).get(0)) + Double.parseDouble(num1.get(0).get(0))));
+                        stack.push(result);
                         break;
                     case '-':
-                        result = Double.parseDouble(num2) - Double.parseDouble(num1);
-                        stack.push(String.valueOf(result));
+                        result.get(0).add(String.valueOf(Double.parseDouble(num2.get(0).get(0)) - Double.parseDouble(num1.get(0).get(0))));
+                        stack.push(result);
                         break;
                     case '•':
-                        result = Double.parseDouble(num2) * Double.parseDouble(num1);
-                        stack.push(String.valueOf(result));
+                        result.get(0).add(String.valueOf(Double.parseDouble(num2.get(0).get(0)) * Double.parseDouble(num1.get(0).get(0))));
+                        stack.push(result);
                         break;
                     case '/':
-                        result = Double.parseDouble(num2) / Double.parseDouble(num1);
-                        stack.push(String.valueOf(result));
+                        result.get(0).add(String.valueOf(Double.parseDouble(num2.get(0).get(0)) / Double.parseDouble(num1.get(0).get(0))));
+                        stack.push(result);
                         break;
                     case '^':
-                        result = Math.pow(Double.parseDouble(num2), Double.parseDouble(num1));
-                        stack.push(String.valueOf(result));
+                        result.get(0).add(String.valueOf(Math.pow(Double.parseDouble(num2.get(0).get(0)), Double.parseDouble(num1.get(0).get(0)))));
+                        stack.push(result);
                         break;
                 }
             }
         }
 
         Log.d(TAG, "Ans: " + stack.pop());
+    }
+
+    public static List<List<String>> getCurrentMatrix(Character Name) {
+
+        int matrixIndex = MatrixCardsController.matrixCardsList.indexOf(String.valueOf(Name));
+
+        return MatrixCardsController.matrixCardsList.get(matrixIndex).getMatrix();
     }
 }
